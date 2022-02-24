5a77dca1065bcb344a7eb9a45602b4ae0dabe6bc
MoonlightDreamer <sdmagomedov@edu.hse.ru>
2022-02-24 23:02:15 +0300

    Splitting quiz controllers and decomposition(sending unv files)

---
+++ src/main/java/com/github/MoonlightDreamer/web/user/AbstractUserQuizController.java
@@ -0,0 +1,64 @@
+package com.github.MoonlightDreamer.web.user;
+
+import com.github.MoonlightDreamer.model.Quiz;
+import com.github.MoonlightDreamer.model.UserQuiz;
+import com.github.MoonlightDreamer.repository.QuizRepository;
+import com.github.MoonlightDreamer.repository.UserQuizRepository;
+import com.github.MoonlightDreamer.util.UserQuizUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+public class AbstractUserQuizController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    protected QuizRepository quizRepository;
+
+    @Autowired
+    protected UserQuizRepository UQRepository;
+
+    public List<Quiz> getAllActiveQuizzes() {
+        log.info("Getting all the active quizzes");
+        return quizRepository.getActiveQuizzes();
+    }
+
+    public ResponseEntity<UserQuiz> takeQuiz(String userAnswers,
+                                             int userId,
+                                             int quizId, String restUrl) {
+        log.info("Saving the answers to the survey {} from user number {}", quizId, userId);
+        UserQuiz created = UQRepository.save(new UserQuiz(userId, quizId, userAnswers));
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(restUrl + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    public List<String> showPassedQuiz(int id,
+                                       int userId) {
+        log.info("showing the answers to the survey {} from user number {}", id, userId);
+        List<String> result;
+        try {
+            result = UserQuizUtil.getListedAnswers(
+                    UQRepository.getQuizResult(id, userId));
+        } catch (RuntimeException e) {
+            return (List.of("No surveys found with such userId"));
+        }
+        return result;
+    }
+
+    public List<List<String>> showAllPassedQuizzes(int userId) {
+        log.info("Loading all the answered surveys from user number {}", userId);
+        try {
+            return UserQuizUtil.getListedAnswers(UQRepository.getAllQuizResults(userId));
+        } catch (RuntimeException e) {
+            return (List.of(List.of("No surveys found with such userId")));
+        }
+    }
+}
---
+++ src/main/java/com/github/MoonlightDreamer/web/user/UnauthorizedController.java
@@ -0,0 +1,32 @@
+package com.github.MoonlightDreamer.web.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(value = UnauthorizedController.REST_API, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UnauthorizedController extends AbstractUserQuizController {
+    static final String REST_API = "api/guest/quizzes";
+
+    @PostMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void takeQuiz(@RequestBody String responses,
+                         @RequestParam int userId,
+                         @PathVariable int id) {
+        super.takeQuiz(responses, userId, id, REST_API);
+    }
+
+    @GetMapping("/story/{id}")
+    public List<String> showPassedQuiz(@PathVariable int id,
+                                       @RequestParam int userId) {
+        return super.showPassedQuiz(id, userId);
+    }
+
+    @GetMapping("/story")
+    public List<List<String>> showAllPassedQuizzes(@RequestParam int userId) {
+        return super.showAllPassedQuizzes(userId);
+    }
+}
